3e97ab50fc7c90f9009c2c17ac9bdb29725d3bf0
mahdiiii04 <mahdidodz2004@gmail.com>
2026-06-06 09:38:52 +0100

    changed pahse length

--- envs/girdworld/envs.py
+++ envs/girdworld/envs.py
@@ -841,7 +841,7 @@ class NSRoleShiftNavEnv(AsymmetricNavEnv):
 
     extra_obs_dim = 2
 
-    def __init__(self, phase_length: int = 1950, **kwargs):  # bring it back to 550 for regular test
+    def __init__(self, phase_length: int = 2550, **kwargs):  # bring it back to 550 for regular test
         self._rs_phase_length = phase_length
         super().__init__(**kwargs)
         self.register_buffer(
